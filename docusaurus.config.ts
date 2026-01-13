93b4fc938eb33f49efdc7b998fd34eb1a9c6819c
Andrew Forster <forsterandrew15@gmail.com>
2026-01-13 07:08:20 -0800

    Updated title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Hexis Documentation',
+  title: 'Hexis Docs',
   tagline: 'Lua scripting API for intelligent Minecraft automation',
   favicon: 'img/favicon.ico',
 
